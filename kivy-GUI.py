d18f12f8e1a29bc9f33f6ae089f1f17b5327fc25
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-12-23 14:42:17 +0000

    color choose checkboxes added.

--- kivy-GUI.py
+++ kivy-GUI.py
@@ -10,6 +10,16 @@ class CheckersLayout(Widget):
     def __init__(self):
         super(CheckersLayout, self).__init__()
 
+    def new_game(self):
+        pass
+
+    def checkbox_active(self, checkbox_1, checkbox_2):
+        if checkbox_1.active:
+            checkbox_2.active = False
+            print(f'The checkbox {checkbox_1}={checkbox_1.active} and {checkbox_2}={checkbox_2.active}')
+        else:
+            print(f'The checkbox {checkbox_1}={checkbox_1.active} and {checkbox_2}={checkbox_2.active}')
+
     def board_field_clicked(self):
         pass
 
@@ -22,6 +32,7 @@ class CheckersLayout(Widget):
 
 class CheckersApp(App):
     def build(self):
+        Window.size = (800, 800)
         Window.clearcolor = (.655, .682, .682, 1)
         return CheckersLayout()
 
