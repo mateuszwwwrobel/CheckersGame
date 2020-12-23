70607e1f3400f62ab4db3304fb3905f40e5dcb46
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-12-23 15:44:13 +0000

    New game button function. Color validation.

--- kivy-GUI.py
+++ kivy-GUI.py
@@ -3,6 +3,8 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+from CheckersClasses import Board
+
 Builder.load_file('checkers.kv')
 
 
@@ -10,15 +12,22 @@ class CheckersLayout(Widget):
     def __init__(self):
         super(CheckersLayout, self).__init__()
 
-    def new_game(self):
-        pass
+    def new_game(self, checkbox_1, checkbox_2):
+        """
+            When clicked initialize new game. According to player choice puts black or white mens
+            on bottom part of the board.
 
-    def checkbox_active(self, checkbox_1, checkbox_2):
+        """
+
         if checkbox_1.active:
-            checkbox_2.active = False
-            print(f'The checkbox {checkbox_1}={checkbox_1.active} and {checkbox_2}={checkbox_2.active}')
+            # black color
+            print('black')
+            game_board = Board()
+            # print(game_board)
+        elif checkbox_2.active:
+            print('white')
         else:
-            print(f'The checkbox {checkbox_1}={checkbox_1.active} and {checkbox_2}={checkbox_2.active}')
+            print('choose a color.')
 
     def board_field_clicked(self):
         pass
