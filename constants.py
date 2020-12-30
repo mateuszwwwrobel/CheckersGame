e429ef5de8c4ff2eba46bdf7120fab62ba32846d
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-12-30 09:38:27 +0000

    add constants.py file

---
+++ constants.py
@@ -0,0 +1,19 @@
+
+# COLORS:
+BLACK = 'black'
+WHITE = 'white'
+
+# PATH TO IMAGES FILES:
+
+BLANK_WHITE = 'img/white_blank.png'
+BLANK_DARK = 'img/black_blank.png'
+
+BLACK_PAWN = 'img/black_black_men.png'
+WHITE_PAWN = 'img/black_white_men.png'
+
+CLICKED_BLACK_PAWN = 'img/clicked_black.png'
+CLICKED_WHITE_PAWN = 'img/clicked_white.png'
+CLICKED_BLANK = 'img/clicked.png'
+
+ICON = 'img/icon.png'
+
